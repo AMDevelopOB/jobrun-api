7cde9b075c0f7a51863ed9992ac271e4506498af
AMDevelop <amdevelop@open-bootcamp.com>
2022-07-12 14:46:53 +0200

    fix: Seeders updated

--- database/seeders/6_FinalSeed.ts
+++ database/seeders/6_FinalSeed.ts
@@ -4,10 +4,155 @@ import { TipoExperiencia } from 'App/Models/Contracts/TipoExperiencia'
 import { TipoJornada } from 'App/Models/Contracts/TipoJornada'
 import { TipoPresencialidad } from 'App/Models/Contracts/TipoPresencialidad'
 import { TipoRegion } from 'App/Models/Contracts/TipoRegion'
+import { TipoRol } from 'App/Models/Contracts/TipoRol'
+import Empresa from 'App/Models/Empresa'
 import Oferta from 'App/Models/Oferta'
+import User from 'App/Models/User'
 
 export default class extends BaseSeeder {
   public async run() {
+    User.createMany([
+      {
+        email: 'amdevelop@open-bootcamp.com',
+        password: 'admin12345',
+        username: 'AMDevelop',
+        nombreCompleto: 'Alejandro Martinez',
+        telefono: '645565464',
+        rol: TipoRol.ADMIN,
+      },
+      {
+        email: 'alex@open-bootcamp.com',
+        password: 'admin12345',
+        username: 'El Master',
+        nombreCompleto: 'Álex Torrentí',
+        telefono: '666333111',
+        rol: TipoRol.ADMIN,
+      },
+      {
+        email: 'joaquin@burguerking.com',
+        password: 'bk12345',
+        username: 'JoaquinSorolla',
+        nombreCompleto: 'Joaquin Sorolla',
+        telefono: '616616616',
+        rol: TipoRol.USER,
+      },
+      {
+        email: 'marialucia@servinform.com',
+        password: 'servinform3321',
+        username: 'MariLuci',
+        nombreCompleto: 'Maria Lucia Gimenez',
+        telefono: '616616619',
+        rol: TipoRol.USER,
+      },
+      {
+        email: 'contacto@open-bootcamp.com',
+        password: 'ob!33',
+        username: 'OpenBootcamp',
+        nombreCompleto: 'Victor Mollá',
+        telefono: '666111222',
+        rol: TipoRol.USER,
+      },
+      {
+        email: 'hola@proxya.com',
+        password: 'proxya1122',
+        username: 'ContactoProxya',
+        nombreCompleto: 'Enrique Hernandez',
+        telefono: '654321321',
+        rol: TipoRol.USER,
+      },
+      {
+        email: 'mariluz@newtalent.com',
+        password: 'ml12345',
+        username: 'MariLuzNT',
+        nombreCompleto: 'Maria Luz Rodríguez',
+        telefono: '678678678',
+        rol: TipoRol.USER,
+      },
+    ])
+
+    await Empresa.createMany([
+      {
+        nombre: 'Burger King S.A.',
+        descripcion:
+          'En este puesto de trabajo aprenderás a manejar la plataforma de colaboración empresarial así como sus productos y elementos de software. Podrás crear sitios de hosting web donde se acceda a espacios de trabajo compartido, almacenando información y documentos, así como alojar blogs o wikis.En este curso aprenderás a manejar la plataforma de colaboración empresarial así como sus productos y elementos de software.En este puesto de trabajo aprenderás a manejar la plataforma de colaboración empresarial así como sus productos y elementos de software.',
+        ubicacion: 'España, Madrid',
+        url: 'https://www.burgerking.es/',
+        userId: 3,
+        enlaceTwitter: 'https://twitter.com/',
+        enlaceLinkedin: 'https://linkedin.com/',
+        enlaceBehance: 'https://behance.com/',
+        numEmpleados: '2500',
+        creacion: '1996',
+      },
+      {
+        nombre: 'Servinform',
+        descripcion:
+          'Servinform somos lideres en servicios digitales y transformación digital 360º. Especialistas en externalización de servicios para empresas.',
+        ubicacion: 'España, Sevilla',
+        url: 'https://www.servinform.es/en/',
+        userId: 4,
+        enlaceTwitter: 'https://twitter.com/',
+        enlaceLinkedin: 'https://linkedin.com/',
+        enlaceBehance: 'https://behance.com/',
+        numEmpleados: '500',
+        creacion: '2003',
+      },
+      {
+        nombre: 'OpenBootcamp',
+        descripcion:
+          'Especialízate en desarrollo independientemente de tu nivel de conocimientos y experiencia, nosotros nos adaptaremos a tus objetivos.',
+        ubicacion: 'España, Valencia',
+        url: 'https://open-bootcamp.com/',
+        userId: 5,
+        enlaceTwitter: 'https://twitter.com/',
+        enlaceLinkedin: 'https://linkedin.com/',
+        enlaceBehance: 'https://behance.com/',
+        numEmpleados: '50',
+        creacion: '2021',
+      },
+      {
+        nombre: 'Proxya',
+        descripcion:
+          'Proxya es una empresa especializada en servicios de BPO, Factoría de Software y Soporte Avanzado en búsqueda de desarrolladores Java que participen en un proyecto para la administración pública.',
+        ubicacion: 'España, Madrid',
+        url: 'https://www.proxya.com/',
+        userId: 6,
+        enlaceTwitter: 'https://twitter.com/',
+        enlaceLinkedin: 'https://linkedin.com/',
+        enlaceBehance: 'https://behance.com/',
+        numEmpleados: '250',
+        creacion: '2002',
+      },
+      {
+        nombre: 'New Talent',
+        descripcion:
+          'Somos una Consultora de Recursos Humanos especializada en atracción de talentos.',
+        ubicacion: 'Argentina, Buenos Aires',
+        url: 'https://newtalent.com.ar/',
+        userId: 7,
+        enlaceTwitter: 'https://twitter.com/',
+        enlaceLinkedin: 'https://linkedin.com/',
+        enlaceBehance: 'https://behance.com/',
+        numEmpleados: '5000',
+        creacion: '2019',
+      },
+    ])
+
+    const empresa1 = await Empresa.findByOrFail('id', 2)
+    await empresa1.related('valores').attach([1, 2])
+
+    const empresa2 = await Empresa.findByOrFail('id', 3)
+    await empresa2.related('valores').attach([1, 3, 4])
+
+    const empresa3 = await Empresa.findByOrFail('id', 4)
+    await empresa3.related('valores').attach([1, 2, 3, 4, 5, 6])
+
+    const empresa4 = await Empresa.findByOrFail('id', 5)
+    await empresa4.related('valores').attach([5, 6])
+
+    const empresa5 = await Empresa.findByOrFail('id', 6)
+    await empresa5.related('valores').attach([1, 6])
+
     await Oferta.createMany([
       {
         nombre: 'Front-End Developer',
